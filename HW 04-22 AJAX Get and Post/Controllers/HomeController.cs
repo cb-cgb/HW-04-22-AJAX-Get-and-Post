53ca00c42189e17458a2eb3792f282bcb7c02566
Chana Becker <cgoldzm@gmail.com>
2020-12-08 14:39:17 -0500

    Add project files.

---
+++ HW 04-22 AJAX Get and Post/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HW_04_22_AJAX_Get_and_Post.Models;
+
+namespace HW_04_22_AJAX_Get_and_Post.Controllers
+{
+    public class HomeController : Controller
+    {          
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HW 04-22 AJAX Get and Post/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HW_04_22_AJAX_Get_and_Post.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
